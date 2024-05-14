ed62067480552ab1e21f9b310ef60d145fa7e93f
Priyanka Dhanabal <priyanka.dhanabal96@gmail.com>
2024-05-14 17:50:18 +0000

    added a model- blog_post

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import blog_post
 
 # Create your views here.
 
 def home(request):
+    context = {
+        'posts': blog_post.objects.all()
+    }
     return render(request, 'blog/home.html')
 
 
