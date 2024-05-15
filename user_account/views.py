446a9dee1050379dff14ee1f597af3693d078ecc
Priyanka Dhanabal <priyanka.dhanabal96@gmail.com>
2024-05-15 19:44:10 +0000

    created a form file anmd added email field to the register page, installed django crispy forms

--- user_account/views.py
+++ user_account/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import user_registration_form
 from django.contrib import messages
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = user_registration_form(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account has been created for {username} !')
             return redirect('blog-home')
     else:
-        form = UserCreationForm()
+        form = user_registration_form()
 
     return render(request, 'user_account/register.html',{'form': form})
