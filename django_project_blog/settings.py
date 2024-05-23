0dbd29e0b158e02b15b0d360fa66c489e5dbe943
Priyanka Dhanabal <priyanka.dhanabal96@gmail.com>
2024-05-23 15:53:01 +0000

    code to deploy in heroku

--- django_project_blog/settings.py
+++ django_project_blog/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3oad*7uresmzzm87if#&#l1&66s*hue4izp*7#h1hyhw4x&-ll'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-priyankadhanabal-pp4-2kgl93970zq.ws-eu114.gitpod.io']
+ALLOWED_HOSTS = ['8000-priyankadhanabal-pp4-2kgl93970zq.ws-eu114.gitpod.io',
+'.herokuapp.com']
 
 
 # Application definition
@@ -42,10 +46,13 @@ INSTALLED_APPS = [
     'user_account',
     'crispy_forms',
     'crispy_bootstrap5',
+    'cloudinary_storage',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-priyankadhanabal-pp4-2kgl93970zq.ws-eu114.gitpod.io',
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com",
 ]
 
 MIDDLEWARE = [
@@ -82,13 +89,14 @@ WSGI_APPLICATION = 'django_project_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
+DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
