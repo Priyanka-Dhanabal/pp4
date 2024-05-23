858a8051fc3e9fcb7642b6a92366835c8f7bcba9
Priyanka Dhanabal <priyanka.dhanabal96@gmail.com>
2024-05-23 20:14:35 +0000

    edited the model for user account and changes made to html files to aplly style

--- user_account/models.py
+++ user_account/models.py
@@ -1,22 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from cloudinary.models import CloudinaryField
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
 class profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    # image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = CloudinaryField(_("Profile Image"), null=True, blank=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self,*args, **kwargs):
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    #     img = Image.open(self.image.path)
+
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
